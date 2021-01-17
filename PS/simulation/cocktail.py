6ff97e04d969c72a8bf8d311c0a6f16b23fe17ce
underflow101 <ikarus125@gmail.com>
2021-01-17 22:52:59 +0900

    feat: Add more PS
    - Added more PS
    
    Signed-off-by: underflow101 <ikarus125@gmail.com>

--- PS/simulation/cocktail.py
+++ PS/simulation/cocktail.py
@@ -1,18 +1,49 @@
 # BOJ 1033
 # cocktail
 
+from math import gcd
+from collections import deque
+
 n = int(input())
-ing = [[0] * n for _ in range(n)]
+ing = [1] * n
+matrix = [[0] * n for _ in range(n)]
+a = [0] * n
+b = [0] * n
+p = [0] * n
+q = [0] * n
 
 for i in range(n-1):
-    a, b, p, q = map(int, input().split())
-    ing[a][b] = p / q
-    ing[b][a] = q / p
+    _a, _b, _p, _q = map(int, input().split())
+    a[i] = _a
+    b[i] = _b
+    p[i] = _p
+    q[i] = _q
+    matrix[a[i]].append(b[i])
+    matrix[b[i]].append(a[i])
+    
+def bfs(start, ban, k):
+    visited = [False] * n
+    queue = deque()
+    queue.append(start)
+    visited[start] = True
+    
+    while queue:
+        curr = queue.popleft()
+        if curr == ban:
+            continue
+        ing[curr] *= k
+        for item in matrix[curr]:
+            if not visited[item]:
+                queue.append(item)
+                visited[item] = True
+                
+for i in range(n-1):
+    bfs(a[i], b[i], p[i])
+    bfs(b[i], a[i], q[i])
+    
+mult = ing[0]
+for i in range(1, n):
+    mult = gcd(mult, ing[i])
 
 for i in range(n):
-    print(ing[i])
-
-for i in range(n):
-    for j in range(n):
-        if ing[i][j] != int(ing[i][j]):
-            ing[i][j]
+    print(ing[i] // mult, end=' ')
