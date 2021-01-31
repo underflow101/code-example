b60ae0919aa4aebc412ec1aaea0373b4371213f4
underflow101 <ikarus125@gmail.com>
2021-01-31 23:46:28 +0900

    feat: Add more PS
    - Added more PS
    
    Signed-off-by: underflow101 <ikarus125@gmail.com>

---
+++ PS/string/chromosome.py
@@ -0,0 +1,42 @@
+# BOJ 9342
+# chromosome
+
+import sys
+#input = sys.stdin.readline
+
+start = ['A', 'B', 'C', 'D', 'E', 'F']
+nextone = ['A']
+nexttwo = ['F']
+nextthree = ['C']
+last = ['A', 'B', 'C', 'D', 'E', 'F']
+check = {'start':False,
+         'nextone':False,
+         'nexttwo':False,
+         'nextthree':False,
+         'last':False}
+
+def check(s):
+    cnt = 0
+    # special case
+    if len(s) > 2:
+        if s[0] == 'A' and s[1] == 'F':
+            for i in range(2, len(s)):
+                if s[i] in nexttwo:
+                    continue
+                else:
+                    if s[i] not in nextthree:
+                        return False
+                    else:
+                        if i == len(s) - 1:
+                            return True
+                        else:
+                            
+                    
+
+T = int(input())
+for _ in range(T):
+    word = input()
+    if len(word) == 0:
+        print("Good")
+    else:
+        
